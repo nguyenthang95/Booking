be8ac58fbed7ca1e31d3575fade4217a5a90599e
Vuong Thuy <thuy.vuong@tpisoftware.com>
2023-02-26 18:10:32 +0700

    commit

---
+++ Booking.Model/Entities/AccountCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booking.Model.Entities
+{
+    public class AccountCategory:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ Booking.Model/Entities/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booking.Model.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Guid CreatedById { get; set; }
+        public Account CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public Guid? UpdatedById { get; set; }
+        public Account UpdatedBy { get; set; }
+    }
+}
